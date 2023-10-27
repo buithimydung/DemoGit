d302960c1a824173d4819b77c14512d8edc30383
buithimydung <146433546+buithimydung@users.noreply.github.com>
2023-10-27 22:13:09 +0700

    Add files via upload

---
+++ CTDL_lab5/src/Task1/Task1_4.java
@@ -0,0 +1,50 @@
+package Task1;
+
+	import java.util.Scanner;
+
+	
+	public class Task1_4 {
+	    public static void main(String[] args) {
+	     
+	       Scanner scanner = new Scanner(System.in);
+	       System.out.println("Enter number of rows in matrix : ");
+	       int rows = scanner.nextInt();
+	       System.out.println("Enter number of columns in matrix : ");
+	       int columns = scanner.nextInt();
+
+	       int matrix[][] = new int[rows][columns];
+
+	       System.out.println("Enter the elements in matrix :");
+	       for (int i = 0; i < rows; i++) {
+	              for (int j = 0; j < columns; j++) {
+	                    matrix[i][j] = scanner.nextInt();
+	              }
+	       }
+
+	       //transpose matrix
+	       int transpose[][] = new int[columns][rows];
+	       for (int i = 0; i < rows; i++) {
+	              for (int j = 0; j < columns; j++)
+	                    transpose[j][i] = matrix[i][j];
+	       }
+
+	       System.out.println("Entered Matrix is : ");
+	       for (int i = 0; i < rows; i++) {
+	              for (int j = 0; j < columns; j++) {
+	                    System.out.print(matrix[i][j] + " ");
+	              }
+	            System.out.println();
+	       }
+
+
+	       System.out.println("Transpose of entered matrix is : ");
+	       for (int i = 0; i < columns; i++) {
+	              for (int j = 0; j < rows; j++)
+	                    System.out.print(transpose[i][j] + " ");
+
+	              System.out.println();
+	       }
+	    }
+	    
+	
+}
